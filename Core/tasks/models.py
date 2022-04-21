c261c0dab830c0a436de76c4d74cabdf14dcc173
godfrey-asiimwe <agtumusiime@gmail.com>
2022-04-21 16:34:19 +0300

    new updates on 21 April 2022

--- Core/tasks/models.py
+++ Core/tasks/models.py
@@ -10,13 +10,13 @@ class Task(models.Model):
     description = models.CharField(max_length=1000, null=True, blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
-    Active_Status = 1
-    Inactive_Status = 0
+    Active_Status = "pending"
+    Inactive_Status = "completed"
     STATUS_CHOICES = (
         (Active_Status, 'pending'),
         (Inactive_Status, 'completed'),
     )
-    status = models.IntegerField(choices=STATUS_CHOICES, default=Active_Status)
+    status = models.CharField(max_length=255,choices=STATUS_CHOICES, default=Active_Status)
     projectId = models.CharField(max_length=255, null=True, blank=True)
     created_by = models.CharField(max_length=255, null=True, blank=True)
     responsible = models.CharField(max_length=255, null=True, blank=True)
